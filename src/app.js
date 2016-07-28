d530638a9125f0964cfc35ffbe00920f8cecb2da
Alexander Kreutz <kreutz@i-tv-t.de>
2016-07-28 14:14:08 +0200

    double code removed

--- src/app.js
+++ src/app.js
@@ -71,29 +71,3 @@ demo.factory('importer', ['$http' , '$translate', 'midataServer', '$q', function
 			
 	return importer;	
 }]);
-// The frontend controller
-demo.controller('MyController', ['$scope', '$translate', '$location', 'midataServer', 'midataPortal', 'importer',  
-	function($scope, $translate, $location, midataServer, midataPortal, importer) {
-		
-	    // Make layout fit into MIDATA page
-	    midataPortal.autoresize();
-	    
-	    // Use language from MIDATA portal
-	    $translate.use(midataPortal.language);
-	    console.log(midataPortal.language);
-	            				
-		// get authorization token from portal
-		var authToken = $location.search().authToken;
-		
-		// Just for demonstration		
-		$scope.worldVar = "hello_world";
-		
-		// button to start import
-		$scope.startImport = function() {
-			importer.importNow(authToken)
-			.then(function() { $scope.status = "ok"; })
-			.catch(function() { $scope.status = "error"; });
-		};
-									
-	}
-]);
